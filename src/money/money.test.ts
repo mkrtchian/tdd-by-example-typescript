b5654fac2331333e92ae247cca71d0474066d61e
Roman Mkrtchian <mkrtchia@gmail.com>
2024-06-24 22:20:12 +0200

    feat(money): additional issues in todo list

--- src/money/money.test.ts
+++ src/money/money.test.ts
@@ -1,5 +1,7 @@
 // $5 + 10CHF = $10 si le taux est de 2:1
 // $5 * 2 = $10
+// Mettre “amount” en privé
+// Quid des side-effects de Dollar ?
 
 it("multiplies money value with given value", () => {
   const five = new Dollar(5);
