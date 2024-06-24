6491703f108ef524e42e2626e0b9327c1fa79cfd
Roman Mkrtchian <mkrtchia@gmail.com>
2024-06-25 01:50:31 +0200

    feat(money): use equals method on the first assertion of multiplication test

--- src/money/money.test.ts
+++ src/money/money.test.ts
@@ -7,7 +7,7 @@ import { Dollar } from "./money";
 it("multiplies money value with given value", () => {
   const five = new Dollar(5);
   let product = five.times(2);
-  expect(product.amount).toBe(10);
+  expect(product.equals(new Dollar(10))).toBe(true);
   product = five.times(3);
   expect(product.amount).toBe(15);
 });
