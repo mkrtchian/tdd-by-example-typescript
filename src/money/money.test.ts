97646ab6130d951b774e3e820896a4244b96632a
Roman Mkrtchian <mkrtchia@gmail.com>
2024-06-26 22:00:32 +0200

    feat(money): add asertions for franc equals

--- src/money/money.test.ts
+++ src/money/money.test.ts
@@ -21,4 +21,6 @@ it("multiplies franc value with given value", () => {
 it("equals to object with the same attributes", () => {
   expect(new Dollar(5).equals(new Dollar(5))).toBe(true);
   expect(new Dollar(5).equals(new Dollar(6))).toBe(false);
+  expect(new Franc(5).equals(new Franc(5))).toBe(true);
+  expect(new Franc(5).equals(new Franc(6))).toBe(false);
 });
