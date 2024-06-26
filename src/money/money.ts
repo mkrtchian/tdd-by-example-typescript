2dfc26635cffddcf18c0cc84593808fdc3dc0e0d
Roman Mkrtchian <mkrtchia@gmail.com>
2024-06-27 00:20:33 +0200

    feat(money): return money from times

--- src/money/money.ts
+++ src/money/money.ts
@@ -4,18 +4,20 @@ class Money {
   }
 
   equals(object: Money) {
-    return this.amount === object.amount && this.constructor === object.constructor;
+    return (
+      this.amount === object.amount && this.constructor === object.constructor
+    );
   }
 }
 
 export class Dollar extends Money {
-  times(multiplier: number) {
+  times(multiplier: number): Money {
     return new Dollar(this.amount * multiplier);
   }
 }
 
 export class Franc extends Money {
-  times(multiplier: number) {
+  times(multiplier: number): Money {
     return new Franc(this.amount * multiplier);
   }
 }
