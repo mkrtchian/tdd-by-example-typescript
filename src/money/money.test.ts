f672530dc19fd29025a341d439a348cd62c51d62
Roman Mkrtchian <mkrtchia@gmail.com>
2024-06-27 00:31:54 +0200

    feat(money): use factory method franc in tests

--- src/money/money.test.ts
+++ src/money/money.test.ts
@@ -14,15 +14,15 @@ it("multiplies dollar value with given value", () => {
 });
 
 it("multiplies franc value with given value", () => {
-  const five = new Franc(5);
-  expect(five.times(2).equals(new Franc(10))).toBe(true);
-  expect(five.times(3).equals(new Franc(15))).toBe(true);
+  const five = Money.franc(5);
+  expect(five.times(2).equals(Money.franc(10))).toBe(true);
+  expect(five.times(3).equals(Money.franc(15))).toBe(true);
 });
 
 it("equals to object with the same attributes", () => {
   expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true);
   expect(Money.dollar(5).equals(Money.dollar(6))).toBe(false);
-  expect(new Franc(5).equals(new Franc(5))).toBe(true);
-  expect(new Franc(5).equals(new Franc(6))).toBe(false);
-  expect(new Franc(5).equals(Money.dollar(5))).toBe(false);
+  expect(Money.franc(5).equals(Money.franc(5))).toBe(true);
+  expect(Money.franc(5).equals(Money.franc(6))).toBe(false);
+  expect(Money.franc(5).equals(Money.dollar(5))).toBe(false);
 });
