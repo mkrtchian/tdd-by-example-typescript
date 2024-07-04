80f05904765b75089f17713195ee5943589cb55f
Roman Mkrtchian <mkrtchia@gmail.com>
2024-07-04 21:38:49 +0200

    feat(money): put back dollar and franc instanciation in factory methods

--- src/money/money.ts
+++ src/money/money.ts
@@ -27,12 +27,12 @@ export abstract class Money {
 
 export class Dollar extends Money {
   times(multiplier: number): Money {
-    return Money.dollar(this.amount * multiplier);
+    return new Dollar(this.amount * multiplier, "USD");
   }
 }
 
 export class Franc extends Money {
   times(multiplier: number): Money {
-    return Money.franc(this.amount * multiplier);
+    return new Franc(this.amount * multiplier, "CHF");
   }
 }
