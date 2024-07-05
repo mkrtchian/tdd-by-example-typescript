61a293fcf041a4e5204fa0e26b937184c6d3a506
Roman Mkrtchian <mkrtchia@gmail.com>
2024-07-05 22:35:17 +0200

    feat(money): check the item on the todo list

--- src/money/money.test.ts
+++ src/money/money.test.ts
@@ -1,7 +1,6 @@
 // $5 + 10CHF = $10 si le taux est de 2:1
 // hashCode()
 // Duplication entre Dollar et Franc
-// times à mettre en commun
 // Supprimer le test de multiplication de Franc
 
 import { Dollar, Money } from "./money";
