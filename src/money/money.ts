34d151ac5097b27c866d23b323a5d192e3fe7d7c
Roman Mkrtchian <mkrtchia@gmail.com>
2024-07-05 22:36:14 +0200

    feat(money): use money instance in factory methods

--- src/money/money.ts
+++ src/money/money.ts
@@ -9,11 +9,11 @@ export class Money {
   }
 
   static dollar(amount: number): Money {
-    return new Dollar(amount, "USD");
+    return new Money(amount, "USD");
   }
 
   static franc(amount: number): Money {
-    return new Franc(amount, "CHF");
+    return new Money(amount, "CHF");
   }
 
   equals(object: Money) {
