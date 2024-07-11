66396c7a8a044c7b489321ab120fb07e1cbc2caa
Roman Mkrtchian <mkrtchia@gmail.com>
2024-07-11 11:45:15 +0200

    empty

--- src/fibonacci/fibonacci.test.ts
+++ src/fibonacci/fibonacci.test.ts
@@ -3,6 +3,8 @@ import { fibonacci } from "./fibonacci";
 it.each([
   [0, 0],
   [1, 1],
-])("return the fibonacci number", (input, expected) => {
+  [2, 1],
+  [3, 2],
+])("return the fibonacci number for %i", (input, expected) => {
   expect(fibonacci(input)).toBe(expected);
 });
--- src/money/expression.ts
+++
@@ -1,8 +0,0 @@
-import { Bank } from "./bank";
-import { Money } from "./money";
-
-export interface Expression {
-  reduce(bank: Bank, to: string): Money;
-  plus(addend: Expression): Expression;
-  times(multiplier: number): Expression;
-}
