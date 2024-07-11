1b54fb0b4d6ed8d842e13df1a84b83740e86aa63
Roman Mkrtchian <mkrtchia@gmail.com>
2024-07-11 12:12:11 +0200

    refactor equality test

--- src/money/money.test.ts
+++ src/money/money.test.ts
@@ -9,3 +9,8 @@ it("should multiply the value", () => {
   const dollar15 = dollar5.times(3);
   expect(dollar15.amount).toBe(15);
 });
+
+it("should ensure equality", () => {
+  expect(new Dollar(1).equals(new Dollar(1))).toBe(true);
+  expect(new Dollar(1).equals(new Dollar(2))).toBe(false);
+});
--- src/money/money.ts
+++ src/money/money.ts
@@ -1,6 +1,8 @@
 /*
 $5 + 10CHF = $10 si le taux est de 2:1
 Mettre “amount” en privé
+--> equals()
+hashCode()
 */
 
 export class Dollar {
@@ -8,5 +10,9 @@ export class Dollar {
 
   times(multiplier: number): Dollar {
     return new Dollar(this.amount * multiplier);
+  }
+
+  equals(dollar: Dollar): boolean {
+    return this.amount === dollar.amount;
   }
 }
